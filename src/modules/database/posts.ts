6859a26247d0cc4b257c706c0eddb8056682d5d3
Toby Cm <toby@vsus.app>
2024-08-14 12:15:11 -0700

    fix: :bug: too much copy paste :skull:

--- src/modules/database/posts.ts
+++ src/modules/database/posts.ts
@@ -3,5 +3,5 @@ import pocketbase from ".";
 import { PBPost } from "./models";
 
 export async function getPosts(options?: RecordListOptions & { page?: number; perPage?: number }) {
-  return await pocketbase.collection<PBPost>("workspaces").getList(options?.page ?? 0, options?.perPage ?? 500, options);
+  return await pocketbase.collection<PBPost>("posts").getList(options?.page ?? 0, options?.perPage ?? 500, options);
 }
